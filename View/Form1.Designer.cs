cee471b677c23dc3f4738aaf96633ab2b93772d0
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-07-06 23:15:16 +0100

    Creating the view

---
+++ View/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace View
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtName = new MetroFramework.Controls.MetroTextBox();
+            this.TxtEmail = new MetroFramework.Controls.MetroTextBox();
+            this.TxtPassword = new MetroFramework.Controls.MetroTextBox();
+            this.BtnGuardar = new System.Windows.Forms.Button();
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.BtnLimpar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TxtName
+            // 
+            // 
+            // 
+            // 
+            this.TxtName.CustomButton.Image = null;
+            this.TxtName.CustomButton.Location = new System.Drawing.Point(315, 1);
+            this.TxtName.CustomButton.Name = "";
+            this.TxtName.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TxtName.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtName.CustomButton.TabIndex = 1;
+            this.TxtName.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtName.CustomButton.UseSelectable = true;
+            this.TxtName.CustomButton.Visible = false;
+            this.TxtName.Lines = new string[0];
+            this.TxtName.Location = new System.Drawing.Point(29, 97);
+            this.TxtName.MaxLength = 32767;
+            this.TxtName.Name = "TxtName";
+            this.TxtName.PasswordChar = '\0';
+            this.TxtName.PromptText = "Nome de Usuário";
+            this.TxtName.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtName.SelectedText = "";
+            this.TxtName.SelectionLength = 0;
+            this.TxtName.SelectionStart = 0;
+            this.TxtName.ShortcutsEnabled = true;
+            this.TxtName.Size = new System.Drawing.Size(337, 23);
+            this.TxtName.TabIndex = 0;
+            this.TxtName.UseSelectable = true;
+            this.TxtName.WaterMark = "Nome de Usuário";
+            this.TxtName.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtName.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // TxtEmail
+            // 
+            // 
+            // 
+            // 
+            this.TxtEmail.CustomButton.Image = null;
+            this.TxtEmail.CustomButton.Location = new System.Drawing.Point(315, 1);
+            this.TxtEmail.CustomButton.Name = "";
+            this.TxtEmail.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TxtEmail.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtEmail.CustomButton.TabIndex = 1;
+            this.TxtEmail.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtEmail.CustomButton.UseSelectable = true;
+            this.TxtEmail.CustomButton.Visible = false;
+            this.TxtEmail.Lines = new string[0];
+            this.TxtEmail.Location = new System.Drawing.Point(29, 144);
+            this.TxtEmail.MaxLength = 32767;
+            this.TxtEmail.Name = "TxtEmail";
+            this.TxtEmail.PasswordChar = '\0';
+            this.TxtEmail.PromptText = "Email";
+            this.TxtEmail.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtEmail.SelectedText = "";
+            this.TxtEmail.SelectionLength = 0;
+            this.TxtEmail.SelectionStart = 0;
+            this.TxtEmail.ShortcutsEnabled = true;
+            this.TxtEmail.Size = new System.Drawing.Size(337, 23);
+            this.TxtEmail.TabIndex = 1;
+            this.TxtEmail.UseSelectable = true;
+            this.TxtEmail.WaterMark = "Email";
+            this.TxtEmail.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtEmail.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // TxtPassword
+            // 
+            // 
+            // 
+            // 
+            this.TxtPassword.CustomButton.Image = null;
+            this.TxtPassword.CustomButton.Location = new System.Drawing.Point(315, 1);
+            this.TxtPassword.CustomButton.Name = "";
+            this.TxtPassword.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.TxtPassword.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
+            this.TxtPassword.CustomButton.TabIndex = 1;
+            this.TxtPassword.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.TxtPassword.CustomButton.UseSelectable = true;
+            this.TxtPassword.CustomButton.Visible = false;
+            this.TxtPassword.Lines = new string[0];
+            this.TxtPassword.Location = new System.Drawing.Point(29, 191);
+            this.TxtPassword.MaxLength = 32767;
+            this.TxtPassword.Name = "TxtPassword";
+            this.TxtPassword.PasswordChar = '\0';
+            this.TxtPassword.PromptText = "Password";
+            this.TxtPassword.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.TxtPassword.SelectedText = "";
+            this.TxtPassword.SelectionLength = 0;
+            this.TxtPassword.SelectionStart = 0;
+            this.TxtPassword.ShortcutsEnabled = true;
+            this.TxtPassword.Size = new System.Drawing.Size(337, 23);
+            this.TxtPassword.TabIndex = 2;
+            this.TxtPassword.UseSelectable = true;
+            this.TxtPassword.WaterMark = "Password";
+            this.TxtPassword.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
+            this.TxtPassword.WaterMarkFont = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Pixel);
+            // 
+            // BtnGuardar
+            // 
+            this.BtnGuardar.BackColor = System.Drawing.SystemColors.Highlight;
+            this.BtnGuardar.FlatAppearance.BorderSize = 0;
+            this.BtnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnGuardar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnGuardar.ForeColor = System.Drawing.Color.White;
+            this.BtnGuardar.Location = new System.Drawing.Point(29, 239);
+            this.BtnGuardar.Name = "BtnGuardar";
+            this.BtnGuardar.Size = new System.Drawing.Size(98, 35);
+            this.BtnGuardar.TabIndex = 3;
+            this.BtnGuardar.Text = "Guardar";
+            this.BtnGuardar.UseVisualStyleBackColor = false;
+            this.BtnGuardar.Click += new System.EventHandler(this.BtnGuardar_Click);
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.AutoSize = true;
+            this.metroLabel1.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.metroLabel1.Location = new System.Drawing.Point(24, 343);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(102, 19);
+            this.metroLabel1.TabIndex = 5;
+            this.metroLabel1.Text = "Criado por Yuki";
+            // 
+            // BtnLimpar
+            // 
+            this.BtnLimpar.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.BtnLimpar.FlatAppearance.BorderSize = 0;
+            this.BtnLimpar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.BtnLimpar.ForeColor = System.Drawing.Color.White;
+            this.BtnLimpar.Location = new System.Drawing.Point(268, 239);
+            this.BtnLimpar.Name = "BtnLimpar";
+            this.BtnLimpar.Size = new System.Drawing.Size(98, 35);
+            this.BtnLimpar.TabIndex = 6;
+            this.BtnLimpar.Text = "Limpar";
+            this.BtnLimpar.UseVisualStyleBackColor = false;
+            this.BtnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(389, 372);
+            this.Controls.Add(this.BtnLimpar);
+            this.Controls.Add(this.metroLabel1);
+            this.Controls.Add(this.BtnGuardar);
+            this.Controls.Add(this.TxtPassword);
+            this.Controls.Add(this.TxtEmail);
+            this.Controls.Add(this.TxtName);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Resizable = false;
+            this.Text = "Cadastro de Utilizadores";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroTextBox TxtName;
+        private MetroFramework.Controls.MetroTextBox TxtEmail;
+        private MetroFramework.Controls.MetroTextBox TxtPassword;
+        private System.Windows.Forms.Button BtnGuardar;
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private System.Windows.Forms.Button BtnLimpar;
+    }
+}
+
---
+++ View/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Model;
+using Controller;
+
+namespace View
+{
+    public partial class Form1 : MetroFramework.Forms.MetroForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            //Limpa todas as Texts Boxs
+            TxtEmail.Clear();
+            TxtName.Clear();
+            TxtPassword.Clear();
+        }
+
+        private void BtnGuardar_Click(object sender, EventArgs e)
+        {
+            //Instancia um novo User 
+            User user = new User();
+
+            //Puxando os Valores do Formulário 
+            user.Name = TxtName.Text;
+            user.Email = TxtEmail.Text;
+            user.Pass = TxtPassword.Text;
+
+            //Instancia um novo Controller
+            UserController ucontroler = new UserController();
+
+            //Adiciona o usuário com os dados do formulário
+            ucontroler.Add(user);
+            
+            //Mensagem de sucesso
+            MetroFramework.MetroMessageBox.Show(this, "Cadastrado com sucesso!", "Sucesso");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
