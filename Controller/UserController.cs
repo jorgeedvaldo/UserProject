855afe02625a80e3f6dbb7f1e3d5083daad7eefc
jorgeedvaldo <edvaldojorge1996@gmail.com>
2020-07-06 23:16:29 +0100

    Creating the controller

---
+++ Controller/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Model;
+
+namespace Controller
+{
+    public class UserController
+    {
+        User _user = new User();
+
+        public void Add(User user)
+        {
+            if (String.IsNullOrWhiteSpace(user.Name))
+            {
+                throw new Exception("Erro, o nome não pode ser vazio.");
+            }
+            else 
+            {
+                _user.Add(user);
+            }
+        }
+    }
+}
